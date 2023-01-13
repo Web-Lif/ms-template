90b35723b12f88d6848549bd0c606762d7fbdbcc
robothot <854363956@qq.com>
2023-01-13 09:03:53 +0800

    feat: 初始化项目

---
+++ .fastpack.ts
@@ -0,0 +1,30 @@
+import FastpackPluginLessLoader from '@weblif/plugin-less-loader'
+import ESLintPlugin from 'eslint-webpack-plugin'
+import { getFastpackConfig } from '@weblif/fastpack'
+
+/**
+ * 扩展 Webpack 的信息
+ */
+class WebpackChainPlugin {
+    after(webpack: any) {
+        webpack.plugin('fastpack/ESLintPlugin').use(ESLintPlugin, [{
+            extensions: ['.ts', '.tsx', '.js', '.jsx']
+        }])
+    }
+}
+
+export default getFastpackConfig({
+    title: 'test',
+    router: {
+        paths: [
+            '/'
+        ]
+    },
+    plugins: [
+        new FastpackPluginLessLoader({}),
+        new WebpackChainPlugin()
+    ],
+    links: [
+        // 'test@http://127.0.0.1:8085/fastpack.share.js'
+    ]
+})
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+
+
+const App: FC = () => {
+    
+    return (
+        <>
+            这个是首页信息 
+        </>
+    )
+    
+}
+
+export default App
